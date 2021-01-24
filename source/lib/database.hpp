0e73832f626ff688748f431f7425cdf16161688b
Joe Wu <wuzongsheng@gmail.com>
2021-01-24 18:30:15 +0800

    reset by keys

--- source/lib/database.hpp
+++ source/lib/database.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <com/variant.hpp>
+#include <log/log.hpp>
 #include <nlohmann/json.hpp>
 #include <shm/buffer.hpp>
 
@@ -13,15 +14,19 @@ class database {
     database(std::string_view name)
         : _buf({ name, "asp" }, 4096) {}
 
-    auto reset(nlohmann::json const& keys, std::vector<com::variant> const& vals) {
+    auto name() const { return _buf.name(); }
+
+    auto reset(nlohmann::json const& keys) {
         std::ostringstream ss;
         ss << keys;
         auto raw = ss.str();
 
-        auto vals_total = sizeof(com::variant) * vals.size();
+        auto vals_count = collect(keys);
+        auto vals_total = sizeof(com::variant) * vals_count;
+        std::vector<com::variant> vals(vals_count, com::variant());
 
-        _buf.resize(vals_total + raw.size());
-        auto l = new (_buf.data()) layout { vals.size() };
+        _buf.resize(sizeof(com::variant) * vals_count + raw.size());
+        auto l = new (_buf.data()) layout { vals_count };
         std::memcpy(l->vals(), vals.data(), vals_total);
         std::strncpy(l->keys(), raw.c_str(), raw.size());
     }
@@ -32,6 +37,20 @@ class database {
     }
 
   private:
+    uint32_t collect(nlohmann::json const& json) const {
+        auto vals = 0U;
+        for (auto const& [key, val] : json.items()) {
+            if (val.is_number_integer()) {
+                vals++;
+            } else if (val.is_structured()) {
+                vals += collect(val);
+            } else {
+                log::error(name(), +"illegal index", key);
+            }
+        }
+        return vals;
+    }
+
     nlohmann::json keys() const {
         auto l = (layout const*)_buf.data();
         return nlohmann::json::parse(l->keys());
--- unittest/ut_database.cpp
+++ unittest/ut_database.cpp
@@ -6,8 +6,17 @@
 #include "source/lib/database.hpp"
 #include "source/lib/layout.hpp"
 
+using miu::com::type_id;
+
 struct ut_database : public testing::Test {
-    void TearDown() override { miu::shm::tempfs::remove("ut_database.asp"); }
+    void SetUp() override {
+        using miu::log::severity;
+        miu::log::log::instance()->reset(severity::DEBUG, 1024);
+    }
+    void TearDown() override {
+        miu::shm::tempfs::remove("ut_database.asp");
+        miu::log::log::instance()->dump();
+    }
 };
 
 TEST_F(ut_database, open) {
@@ -29,27 +38,28 @@ TEST_F(ut_database, open) {
     }
 
     miu::asp::database db { "ut_database" };
+    EXPECT_EQ("ut_database.asp", db.name());
     EXPECT_EQ(1, db.get("item0").get<int32_t>());
     EXPECT_EQ("abc", db.get("item1").get<std::string>());
 }
 
 TEST_F(ut_database, reset) {
     nlohmann::json keys;
-    keys["item0"] = 0;
-    keys["item1"] = 1;
+    keys["item0"]             = 0;
+    keys["item1"]             = 1;
+    keys["group"]["item1"]    = 2;
+    keys["array"][0]["item1"] = 3;
+    keys["array"][1]["item1"] = 0.1;
 
-    std::vector<miu::com::variant> vals { 1, std::string("abc") };
-
-    {
-        miu::asp::database db { "ut_database" };
-        db.reset(keys, vals);
-    }
+    miu::asp::database { "ut_database" }.reset(keys);
 
     miu::shm::buffer buf { "ut_database.asp" };
     auto layout = (miu::asp::layout*)buf.data();
-    EXPECT_EQ(2U, layout->size);
-    EXPECT_EQ(1, layout->vals()[0].get<int32_t>());
-    EXPECT_EQ("abc", layout->vals()[1].get<std::string>());
+    EXPECT_EQ(4U, layout->size);
+    EXPECT_EQ(type_id<void>::value, layout->vals()[0].id());
+    EXPECT_EQ(type_id<void>::value, layout->vals()[1].id());
+    EXPECT_EQ(type_id<void>::value, layout->vals()[2].id());
+    EXPECT_EQ(type_id<void>::value, layout->vals()[3].id());
 
     std::ostringstream ss;
     ss << keys;
