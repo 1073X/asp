f8e5a253d665dddf50b69a58f1daeaabf18f0969
Joe Wu <wuzongsheng@gmail.com>
2021-01-25 23:53:33 +0800

    support array

--- source/lib/frontend.hpp
+++ source/lib/frontend.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
+#include <com/fatal_error.hpp>
 #include <com/variant.hpp>
 #include <functional>
+#include <log/log.hpp>
 #include <nlohmann/json.hpp>
 #include <vector>
 
@@ -19,17 +21,34 @@ class frontend {
 
     template<typename T, typename... ARGS>
     auto insert(callback_type const& callback, T const& t, ARGS&&... args) {
-        make_index(_keys, t, std::forward<ARGS>(args)...);
+        make_index("root", _keys, t, std::forward<ARGS>(args)...);
         _callbacks.push_back(callback);
     }
 
   private:
-    void make_index(nlohmann::json& root) const { root = _callbacks.size(); }
+    void make_index(com::strcat const& path, nlohmann::json& root) const {
+        if (root.is_null()) {
+            root = _callbacks.size();
+            log::debug(+"asp", _callbacks.size(), path);
+        } else {
+            FATAL_ERROR<std::logic_error>("asp(dup)", path.str());
+        }
+    }
 
     template<typename T, typename... ARGS>
-    void make_index(nlohmann::json& root, T const& t, ARGS&&... args) const {
+    void
+    make_index(com::strcat const& path, nlohmann::json& root, T const& t, ARGS&&... args) const {
         auto name = com::to_string(t);
-        make_index(root[name], std::forward<ARGS>(args)...);
+        make_index({ path, name }, root[name], std::forward<ARGS>(args)...);
+    }
+
+    template<typename... ARGS>
+    void
+    make_index(com::strcat const& path, nlohmann::json& root, int32_t idx, ARGS&&... args) const {
+        while (idx >= root.size()) {
+            root.push_back(nlohmann::json());
+        }
+        make_index({ path, idx }, root[idx], std::forward<ARGS>(args)...);
     }
 
   private:
--- unittest/ut_frontend.cpp
+++ unittest/ut_frontend.cpp
@@ -6,8 +6,14 @@ using miu::com::variant;
 using testing::Return;
 
 struct ut_frontend : public testing::Test {
-    MOCK_METHOD(variant, callback, ());
+    void SetUp() override {
+        using miu::log::severity;
+        miu::log::log::instance()->reset(severity::DEBUG, 1024);
+    }
 
+    void TearDown() override { miu::log::log::instance()->dump(); }
+
+    MOCK_METHOD(variant, callback, ());
     miu::asp::frontend frontend;
 };
 
@@ -16,19 +22,43 @@ TEST_F(ut_frontend, insert) {
     frontend.insert(callback, "item1");
     frontend.insert(callback, "item2");
     frontend.insert(callback, "group", "item1");
+    frontend.insert(callback, "array", 0, "item1");
+    frontend.insert(callback, "array", 3);
+    frontend.insert(callback, "array", 2, "item1");
+    frontend.insert(callback, "array", 0, "item2");
 
-    EXPECT_EQ(3U, frontend.size());
+    EXPECT_EQ(7U, frontend.size());
     EXPECT_CALL(*this, callback())
         .WillOnce(Return(variant(1)))
         .WillOnce(Return(variant(+"abc")))
-        .WillOnce(Return(variant(1.2)));
+        .WillOnce(Return(variant(1.2)))
+        .WillOnce(Return(variant(2)));
     EXPECT_EQ(variant(1), frontend[0]());
     EXPECT_EQ(variant(+"abc"), frontend[1]());
     EXPECT_EQ(variant(1.2), frontend[2]());
+    EXPECT_EQ(variant(2), frontend[3]());
 
     nlohmann::json keys;
-    keys["item1"]          = 0;
-    keys["item2"]          = 1;
-    keys["group"]["item1"] = 2;
+    keys["item1"]             = 0;
+    keys["item2"]             = 1;
+    keys["group"]["item1"]    = 2;
+    keys["array"][0]["item1"] = 3;
+    keys["array"][0]["item2"] = 6;
+    keys["array"][1]          = nlohmann::json();
+    keys["array"][2]["item1"] = 5;
+    keys["array"][3]          = 4;
     EXPECT_EQ(keys, frontend.keys());
 }
+
+TEST_F(ut_frontend, duplicated) {
+    auto callback = std::bind(&ut_frontend::callback, this);
+    frontend.insert(callback, "item1");
+    EXPECT_ANY_THROW(frontend.insert(callback, "item1"));
+
+    frontend.insert(callback, "group", "item1");
+    EXPECT_ANY_THROW(frontend.insert(callback, "group", "item1"));
+
+    frontend.insert(callback, "array", 0);
+    EXPECT_ANY_THROW(frontend.insert(callback, "array", 0));
+}
+
