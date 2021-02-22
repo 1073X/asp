a4362961162709f35e9d4776bcf1f2478e16b091
Joe Wu <wuzongsheng@gmail.com>
2021-02-22 21:26:43 +0800

    apply new time

--- source/lib/var_json.cpp
+++ source/lib/var_json.cpp
@@ -1,6 +1,7 @@
 
 #include <com/variant.hpp>
 #include <nlohmann/json.hpp>
+#include <time/stamp.hpp>
 
 using json = nlohmann::json;
 
@@ -13,16 +14,16 @@ std::optional<json> variant::get<json>() const {
     case type_id<int16_t>::value:
     case type_id<int32_t>::value:
     case type_id<int64_t>::value:
-    case type_id<days>::value:
-    case type_id<microseconds>::value:
+    case type_id<time::days>::value:
+    case type_id<time::delta>::value:
         return json(get<int64_t>().value());
     case type_id<uint8_t>::value:
     case type_id<uint16_t>::value:
     case type_id<uint32_t>::value:
     case type_id<uint64_t>::value:
-    case type_id<date>::value:
-    case type_id<daytime>::value:
-    case type_id<datetime>::value:
+    case type_id<time::date>::value:
+    case type_id<time::daytime>::value:
+    case type_id<time::stamp>::value:
         return json(get<uint64_t>().value());
     case type_id<float>::value:
     case type_id<double>::value:
